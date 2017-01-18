5c86b5b92eb4c84a0e5ff9ece5c176b5a9f22390
j-west <westja89@gmail.com>
2017-01-18 13:52:27 -0600

    Move app.config to it's own file

---
+++ app/config.js
@@ -0,0 +1,19 @@
+app.config(($routeProvider, $locationProvider) => {
+  $locationProvider.hashPrefix("");
+  $routeProvider
+    .when("/", {
+      controller: "FirstCtrl",
+      templateUrl: "app/partials/firstpartial.html",
+    })
+    .when("/second", {
+      controller: "SecondCtrl",
+      templateUrl: "app/partials/secondpartial.html",
+    })
+    .when("/third", {
+      controller: "ThirdCtrl",
+      templateUrl: "app/partials/thirdpartial.html",
+    })
+    .otherwise({
+      redirectTo: "/"
+    });
+});
