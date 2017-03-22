01eb44319e7a5ddc284882d7e33e2346ff062ed0
j-west <westja89@gmail.com>
2017-03-22 11:14:02 -0500

    Add error handler during readFile

--- server01.js
+++ server01.js
@@ -7,6 +7,11 @@ const server = createServer();
 
 server.on('request', (req, res) => {
   readFile('./index.html', (err, buff) => {
+    if (err) {
+      res.statusCode = 400;
+      console.log(res);
+      return res.end('404 Not Found')
+    }
     res.end(buff);
   });
 });
